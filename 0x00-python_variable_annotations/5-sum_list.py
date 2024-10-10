e678851f08b201aa62e3ef15ffb6fb5ad0370ecf
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-10 18:09:07 +0300

    p

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 """module to sum list of floats"""
 
 
-def sum_list(input_list: [float]) -> float:
-    """Returns the sum of the list of floats."""
+def sum_list(input_list: List[float]) -> float:
+    """function to sum a list of floats"""
     return sum(input_list)
