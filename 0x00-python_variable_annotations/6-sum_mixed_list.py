c64c722ee16c140f07cac4aa02e3e16558ceb22c
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-10 18:10:35 +0300

    p

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module to sum a list of mixed floats and ints"""
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """function to sum a list of mixed floats and ints"""
+    return sum(mxd_lst)
