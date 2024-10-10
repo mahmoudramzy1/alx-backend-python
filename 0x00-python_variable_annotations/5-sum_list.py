291d23d45797b23e06daabb2a58557dde1b51402
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-11 01:22:22 +0300

    p

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """module to sum list of floats"""
+from typing import List
 
 
-def sum_list(input_list: List[float]) -> float:
+def sum_list(input_list: List [float]) -> float:
     """function to sum a list of floats"""
     return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """module to sum a list of mixed floats and ints"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function to sum a list of mixed floats and ints"""
     return sum(mxd_lst)
