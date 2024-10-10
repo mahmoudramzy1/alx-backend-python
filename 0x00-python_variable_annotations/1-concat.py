62f0be4e1a134b4e6fd1a9990182280787763ce8
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-10 17:23:46 +0300

    p

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module to concat two strings with thier annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function to concat two strings"""
+    return str1 + str2
